86f7d868d2fe74fb47907e37170889845cd50792
Nelson-Chinedu <egwuchinedu69@gmail.com>
2025-05-02 21:21:35 +0100

    feat: get an employee

--- src/modules/employee/__mocks__/employee.service.ts
+++ src/modules/employee/__mocks__/employee.service.ts
@@ -1,6 +1,10 @@
-import { responseStub } from '../test/stubs/employee.stubs';
+import {
+  employeeResponseStub,
+  responseStub,
+} from '../test/stubs/employee.stubs';
 
 export const EmployeeService = jest.fn().mockReturnValue({
   create: jest.fn().mockResolvedValue(responseStub()),
-  getAllEmployees: jest.fn().mockResolvedValue([responseStub()]),
+  getAllEmployees: jest.fn().mockResolvedValue([employeeResponseStub()]),
+  getEmployee: jest.fn().mockResolvedValue(employeeResponseStub()),
 });
--- src/modules/employee/test/stubs/employee.stubs.ts
+++ src/modules/employee/test/stubs/employee.stubs.ts
@@ -15,3 +15,20 @@ export const responseStub = () => {
     message: 'Employee added successfully',
   };
 };
+
+export const employeeResponseStub = () => {
+  return {
+    id: '537ed35f-d6b8-46cb-963c-b7b1631b2a2f',
+    firstname: 'Test',
+    lastname: 'test',
+    phone_number: '+2348123901239',
+    department: 'design',
+    job_title: 'Creative Designer',
+    contract_type: 'Full time',
+    account: {
+      email: 'test@email.com',
+    },
+    createdAt: '2025-05-02T17:05:54.933Z',
+    updatedAt: '2025-05-02T17:05:54.933Z',
+  };
+};
